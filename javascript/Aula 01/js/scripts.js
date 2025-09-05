cb01508009954fd9db75dcc1e074f68485fe1c40
ThyagoAlmeidaCode <thyago.assis@senacrio.com.br>
2025-09-05 13:52:49 -0300

    Js

---
+++ javascript/Aula 01/js/scripts.js
@@ -0,0 +1,26 @@
+/* alert("Ola mundo!"); */ 
+document.write("Minha Idade é:") /* Não recomendado */
+document.write("Ola mundo!");
+console.log("Ola mundo");
+
+/* Variaáveis - São como caixas onde guardamos coisas (Informação) */
+/* var - forma mais antiga */
+var nome = "Thiago";
+
+/* let - forma mais atual */
+let sobrenome = "Silva";
+let sobrenome_um = prompt("Qual seu sobrenome?");
+/* const - constante  - variaveis cujo valor não pode ser trocados*/
+const idade = 25;
+console.log("Ola " + nome + sobrenome) ;
+
+/* Tipos de dados  */
+
+/* String  - texto*/
+/* Number - numeros*/
+/* Boolean - true ou false*/
+
+let classificação = 25;
+console.log(typeof classificação);
+
+
