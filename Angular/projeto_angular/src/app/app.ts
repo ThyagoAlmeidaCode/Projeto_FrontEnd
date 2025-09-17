108c4331ae46ab410d616201de15786f3b0644bb
ThyagoAlmeidaCode <thyago.assis@senacrio.com.br>
2025-09-16 21:16:42 -0300

    angular

--- Angular/projeto_angular/src/app/app.ts
+++ Angular/projeto_angular/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PerfilComponent } from "./perfil/perfil";
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
